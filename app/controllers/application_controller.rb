de2fa20a8b3fa50abf4268880fc3ee9c7ebc659d
cindy <ciindy.wu@gmail.com>
2018-04-17 09:54:08 -0700

    message for alo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def hello
-  	render html: "hello world!"
+  	render html: "Hello Alison! Welcome to RYZA, the yoga studio of your dreams."
   end
 end
